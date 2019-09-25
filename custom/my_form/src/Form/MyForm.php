c6cffb4c1f3ed700f44e1812c4d7f7723bcc203c
Shyk400541 <killyouall@bk.ru@>
2019-09-25 08:58:51 +0300

    Fixed

--- custom/my_form/src/Form/MyForm.php
+++ custom/my_form/src/Form/MyForm.php
@@ -52,34 +52,91 @@ class MyForm extends FormBase {
 
 		if(empty($form_state->getValue('first_name'))){
 		$this->messenger()->addStatus($this->t('Field "First Name" must be not empty'));
+		return;
 		}
 
 		if(empty($form_state->getValue('last_name'))){
 		$this->messenger()->addStatus($this->t('Field "Last Name" must be not empty'));
+		return;
 		}
 
 		if(empty($form_state->getValue('subject'))){
 		$this->messenger()->addStatus($this->t('Field "Subject" must be not empty'));
+		return;
 		}
 
 		if(empty($form_state->getValue('message'))){
 		$this->messenger()->addStatus($this->t('Field "Message" must be not empty'));
+		return;
 		}
 
 		if(empty($form_state->getValue('email'))){
 		$this->messenger()->addStatus($this->t('Field "Email" must be not empty'));
+		return;
 		}
-		else if(filter_var($form_state->getValue('email'), FILTER_VALIDATE_EMAIL)){
-		$this->messenger()->addStatus($this->t('Your email is @email',['@email'=>$form_state->getValue('email')]));
-		}
-		else $this->messenger()->addStatus($this->t('Your email is incorrect'));
 
+		else if((filter_var($form_state->getValue('email'), FILTER_VALIDATE_EMAIL)) == FALSE){
+			$this->messenger()->addStatus($this->t('Your email is incorrect'));
+			return;
+		}
+		//$this->messenger()->addStatus($this->t('Your email is @email',['@email'=>$form_state->getValue('email')]));
 	}
 
 	/**
 	*{@inheritdoc}
 	*/
 	public function submitForm(array &$form, FormStateInterface $form_state){
+
+		$this->send_mail($form, $form_state);
+		
+		$this->create_contact($form, $form_state);
+
+		return;
+	}
+
+	/**
+ 	* Implements hook_mail().
+ 	*/
+	public function my_form_mail($key, &$message, $params){
+
+		$options = ['langcode' => $message['langcode'],];
+
+		switch ($key) {
+			case 'submit':
+				$message['from'] = \Drupal::config('system.site')->get('mail');
+				$message['subject'] = Html::escape($params['subject']);
+				$message['body'][] = Html::escape($params['message']);
+				break;
+		}
+		return;
+	}
+
+	public function create_contact(array &$form, FormStateInterface $form_state){
+
+		$email = $form_state->getValue('email');
+		$firstname = $form_state->getValue('first_name');
+		$lastname = $form_state->getValue('last_name');
+
+		$url = "https://api.hubapi.com/contacts/v1/contact/createOrUpdate/email/".$email."/?hapikey=ed189448-eb49-4e06-bbcc-b0f68d5fa75d";
+
+		$data = array ('properties' => [
+			['property' => 'firstname',
+			'value' => $firstname],
+			['property' => 'lastname',
+			'value' => $lastname]
+		]);
+
+		if(($json = json_encode($data, true)) == FALSE){
+			$this->messenger()->addStatus($this->t('Json encoding is fail'));
+		}
+
+		$request = \Drupal::httpClient()->post($url.'&_format=json',['headers' => ['Content-Type' => 'application/json'], 'body' => $json]);
+		//$response = json_decode($request->getBody());
+		//$this->messenger()->addStatus($this->t($response));
+		return;
+	}
+
+	public function send_mail(array &$form, FormStateInterface $form_state){
 
 		$mailManager = \Drupal::service('plugin.manager.mail');
 		$module = 'my_form';
@@ -93,23 +150,15 @@ class MyForm extends FormBase {
 		$result = $mailManager->mail($module, $key, $to, $langcode, $params, NULL, $send);
 		if($result['result'] != true) {
 			$this->messenger()->addStatus($this->t('There was a problem sending your email'));
-			return;
 		}
 		
-		else $this->messenger()->addStatus($this->t('Your email has been sent'));
-		return;
-	}
-
-	public function my_form_mail($key, &$message, $params){
-
-		$options = ['langcode' => $message['langcode'],];
-
-		switch ($key) {
-			case 'submit':
-				$message['from'] = \Drupal::config('system.site')->get('mail');
-				$message['subject'] = Html::escape($params['subject']);
-				$message['body'][] = Html::escape($params['message']);
-				break;
+		else {
+			$this->messenger()->addStatus($this->t('Your email has been sent'));
+			$file = 'log.txt';
+			$message = date('l jS \of F Y h:i:s A').''.'Mail has been send to '.$form_state->getValue('email');
+			file_put_contents($file, $message, FILE_APPEND);
 		}
+
+		return;
 	}
 }
